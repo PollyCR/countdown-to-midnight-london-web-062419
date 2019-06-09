f7442a4a99ecc635ca9b8cf30de19b4f7992e71b
Polly Reynolds <polly.c.reynolds@gmail.com>
2019-06-09 12:42:15 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,10 @@
 #write your code here
 
 def countdown(x)
-  while x > 0 
+  while x > 0 do
   puts "#{x} SECOND(S)!"
   x -= 1 
-  if x = 0 
-    puts "HAPPY NEW YEAR!"
-  end 
+
 end 
 
 def countdown_with_sleep(x)
